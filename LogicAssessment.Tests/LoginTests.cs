e0a65e6ad479c9e3272a44d441339dee226c3812
Stefano Germani <stefger88@gmail.com>
2016-03-15 22:22:13 +0000

    removed unnecessary usings

--- LogicAssessment.Tests/LoginTests.cs
+++ LogicAssessment.Tests/LoginTests.cs
@@ -1,10 +1,5 @@
 ﻿using Nancy;
 using Nancy.Testing;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace LogicAssessment.Tests
