00c5587047438b871a20580897c35698db7b1a22
Stefano Germani <stefger88@gmail.com>
2016-03-15 19:53:55 +0000

    generating password

---
+++ LogicAssessment.Tests/Tests.cs
@@ -0,0 +1,41 @@
+﻿using Nancy;
+using Nancy.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LogicAssessment.Tests
+{
+    public class Tests
+    {
+        class User
+        {
+            public int UserId { get; set; }
+        }
+
+        [Fact]
+        public void generate_user_password()
+        {
+            var browser = new Browser(with => with.Module<UserModule>());
+
+            var result = GeneratePassword(browser);
+
+            Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+            Assert.False(string.IsNullOrEmpty(result.Body.AsString()));
+        }
+
+
+
+        private static BrowserResponse GeneratePassword(Browser browser)
+        {
+            return browser.Post("/generatePassword", with =>
+            {
+                with.HttpRequest();
+                with.JsonBody(new User() { UserId = 12345 });
+            });
+        }
+    }
+}
---
+++ LogicAssessment/UserModule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicAssessment
+{
+    public class UserModule : Nancy.NancyModule
+    {
+        public UserModule()
+        {
+            Post["/generatePassword"] = user => {
+                    return Guid.NewGuid().ToString();
+                };
+        }
+    }
+
+}
